c0046a6ed061d83d8ec3abb570f82969f02c6a38
agcacruz <micahluis@yahoo.com.ph>
2018-12-27 22:11:07 +0800

    minor adjustments to navbar

--- front/src/component/navigation.js
+++ front/src/component/navigation.js
@@ -1,12 +1,62 @@
 import React from 'react';
-import {NavLink } from 'react-router-dom';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
 
-const Navigation = () => {
-    return(
-        <div>
-            <NavLink to="/">Home</NavLink>
-        </div>
-    )
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Home</NavbarBrand>
+          <NavbarBrand href="/login">Login</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components">Components</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Profile
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Settings
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Logout
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
-
-export default Navigation
--- front/src/pages/error.js
+++ front/src/pages/error.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { Jumbotron, Container, Alert } from 'reactstrap';
 
 const Error = () => {
     return(
-        <div>
-            <p>
-                Error: Path does not exist
-            </p>
-        </div>
+        <div className="content-wrapper">
+                <section className="content">
+                    <div className="error-page">
+                        <Jumbotron fluid>
+                            <Container fluid>
+                                <h1 className="display-3">404</h1>
+                                <div className="error-content">
+                                    <Alert color="warning">Oops! Page not found.</Alert>
+                                    <p className="lead">
+                                        We could not find the page you were looking for.<br/>
+                                        Meanwhile, you may <a href="/">return to home.</a><br/><br/>
+                                        {/* If you think this is wrong, please contact the administrator at <a href="mailto:qbosadmin@questronix.com.ph">qbosadmin@questronix.com.ph</a>. */}
+                                    </p>
+                                </div>
+                            </Container>
+                        </Jumbotron>
+                        
+                    </div>
+                </section>
+            </div>
     )
 }
 
