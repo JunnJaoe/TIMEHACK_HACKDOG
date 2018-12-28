6d61636f7ef4fdd90ab6bd5b0b419f6f48a0e80e
unknown <vhid.abaiz@gmail.com>
2018-12-28 10:45:48 +0800

    Added CoreUi Template

--- front/src/pages/home.js
+++ front/src/pages/home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../component/css/home.css";
+import Navigation from '../component/navigation';
 
 const Home = () => {
     return(
         <div className="Home">
+            <Navigation/>
             <div className="lander">
                 <h1>Home</h1>
             </div>
